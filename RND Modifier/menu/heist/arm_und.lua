03918683fbd886d982f26a4f22b5be44e5867ac0
Dr_Newbie <tf2baidonation@gmail.com>
2017-05-10 00:51:02 +0800

    3.2 ; reduce loc

--- RND Modifier/menu/heist/arm_und.lua
+++ RND Modifier/menu/heist/arm_und.lua
@@ -13,15 +13,15 @@ MenuCallbackHandler.RNDModifier_arm_und_rand_truck_amount = function(self, item)
 end
 MenuHelper:AddMultipleChoice({
 	id = "RNDModifier_".. _Curret_Heist .."_rand_truck_amount",
-	title = "RNDModifier_".. _Curret_Heist .."_rand_truck_amount_title",
+	title = "RNDModifier_arm_rand_truck_amount_title",
 	desc = "RNDModifier_empty_desc",
 	callback = "RNDModifier_".. _Curret_Heist .."_rand_truck_amount",
 	items = {
 		"RNDModifier_Default_One_Item",
-		"RNDModifier_arm_und_rand_truck_amount_1",
-		"RNDModifier_arm_und_rand_truck_amount_2",
-		"RNDModifier_arm_und_rand_truck_amount_3",
-		"RNDModifier_arm_und_rand_truck_amount_4",
+		"RNDModifier_number_4_use_1",
+		"RNDModifier_number_4_use_2",
+		"RNDModifier_number_4_use_3",
+		"RNDModifier_number_4_use_4",
 	},
 	value = RNDModifier._data[_Curret_Heist]._rand_truck_amount,
 	menu_id = "RNDModifier_".. _Curret_Heist .."_Options_Menu"
