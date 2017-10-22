8fd0ebcf1e03a02dfe897bdcfdde7adfd706ef9a
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-22 20:03:46 +0800

    6.3

--- assets/mod_overrides/RNGModifier/Hooks/lua/ForcedEscapeDay.lua
+++ assets/mod_overrides/RNGModifier/Hooks/lua/ForcedEscapeDay.lua
@@ -4,6 +4,8 @@ if not RNGModifier then
 	return
 end
 
+local _forced_escape_day = false
+
 Hooks:PostHook(GroupAIStateBase, "set_whisper_mode", "RNGModifier_GroupAIStateBase_set_whisper_mode", function(self, enable)
 	if not enable and not self._whisper_mode and not _forced_escape_day and RNGModifier:SafeGetData("all_of_all", "_forced_escape_day") == 1 then
 		_forced_escape_day = true
