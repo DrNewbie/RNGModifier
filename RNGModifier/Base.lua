cb5210f79bc412be794a02b2406d24652c7525e3
Almog <almogveg@gmail.com>
2020-08-23 13:43:55 +0300

    stop removing old savefile every update, update it instead.

--- RNGModifier/Base.lua
+++ RNGModifier/Base.lua
@@ -234,10 +234,12 @@ function RNGModifier:Load()
 		_data = json.decode(_file:read("*all" ))
 		_file:close()
 	end
-	if not _data or not _file or tostring(self._data["Version"]) ~= tostring(_data["Version"]) then
+	if not _data or not _file or tostring(self._data["Version"]) < tostring(_data["Version"]) then -- this will break if version number count will change digits amount, but good enough for now
 		self:Save()
 	else
+		local version = self._data["Version"]
 		self._data = _data
+		self._data["Version"] = version
 	end
 end
 
