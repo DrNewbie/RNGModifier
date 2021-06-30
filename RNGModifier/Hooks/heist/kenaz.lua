0372e25b811223dda6c7c14eb8215ce3f9dbde43
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2021-06-30 01:36:41 -0300

    14.4

--- RNGModifier/Hooks/heist/kenaz.lua
+++ RNGModifier/Hooks/heist/kenaz.lua
@@ -2,217 +2,66 @@ _G.RNGModifier = _G.RNGModifier or {}
 if not RNGModifier then
 	return
 end
-local _Curret_Heist = "kenaz"
+local _Curret_Heist = "hvh"
 RNGModifier._data = RNGModifier._data or {}
 RNGModifier._data[_Curret_Heist] = RNGModifier._data[_Curret_Heist] or {}
 
-MenuCallbackHandler.RNGModifier_kenaz_gambler = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_gambler")
+MenuCallbackHandler.RNGModifier_hvh_rnd_world = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_rnd_world")
 	RNGModifier:Save()
 end
 MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_gambler",
-	title = "RNGModifier_kenaz_gambler_title",
+	id = "RNGModifier_hvh_rnd_world",
+	title = "RNGModifier_hvh_rnd_world_title",
 	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_gambler",
+	callback = "RNGModifier_hvh_rnd_world",
 	items = {
 		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_gambler_1",
-		"RNGModifier_kenaz_gambler_2"
+		"RNGModifier_hvh_rnd_world_1",
+		"RNGModifier_hvh_rnd_world_2",
+		"RNGModifier_hvh_rnd_world_3",
+		"RNGModifier_hvh_rnd_world_4"
 	},
-	value = RNGModifier:SafeGetData("kenaz", "_gambler"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
+	value = RNGModifier:SafeGetData("hvh", "_rnd_world"),
+	menu_id = "RNGModifier_hvh_Options_Menu"
 })
 
-MenuCallbackHandler.RNGModifier_kenaz_spiked_colour = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_spiked_colour")
+MenuCallbackHandler.RNGModifier_hvh_vent = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_vent")
 	RNGModifier:Save()
 end
 MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_spiked_colour",
-	title = "RNGModifier_kenaz_spiked_colour_title",
+	id = "RNGModifier_hvh_vent",
+	title = "RNGModifier_hvh_vent_title",
 	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_spiked_colour",
+	callback = "RNGModifier_hvh_vent",
 	items = {
 		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_spiked_colour_1",
-		"RNGModifier_kenaz_spiked_colour_2",
-		"RNGModifier_kenaz_spiked_colour_3",
-		"RNGModifier_kenaz_spiked_colour_4"
+		"RNGModifier_hvh_vent_1",
+		"RNGModifier_hvh_vent_2",
+		"RNGModifier_hvh_vent_3"
 	},
-	value = RNGModifier:SafeGetData("kenaz", "_spiked_colour"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
+	value = RNGModifier:SafeGetData("hvh", "_vent"),
+	menu_id = "RNGModifier_hvh_Options_Menu"
 })
 
-MenuCallbackHandler.RNGModifier_kenaz_vomit_spot = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_vomit_spot")
+MenuCallbackHandler.RNGModifier_hvh_safe = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_safe")
 	RNGModifier:Save()
 end
 MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_vomit_spot",
-	title = "RNGModifier_kenaz_vomit_spot_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_vomit_spot",
+	id = "RNGModifier_hvh_safe",
+	title = "RNGModifier_hvh_safe_title",
+	desc = "RNGModifier_hvh_safe_desc",
+	callback = "RNGModifier_hvh_safe",
 	items = {
 		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_vomit_spot_1",
-		"RNGModifier_kenaz_vomit_spot_2"
+		"RNGModifier_hvh_safe_1",
+		"RNGModifier_hvh_safe_2",
+		"RNGModifier_hvh_safe_3",
+		"RNGModifier_hvh_safe_4",
+		"RNGModifier_hvh_safe_5"
 	},
-	value = RNGModifier:SafeGetData("kenaz", "_vomit_spot"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_easy_code = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_easy_code")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_easy_code",
-	title = "RNGModifier_kenaz_easy_code_title",
-	desc = "RNGModifier_kenaz_easy_code_desc",
-	callback = "RNGModifier_kenaz_easy_code",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_bool_4_true"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_easy_code"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_gear_location = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_gear_location")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_gear_location",
-	title = "RNGModifier_kenaz_gear_location_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_gear_location",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_gear_location_1",
-		"RNGModifier_kenaz_gear_location_2",
-		"RNGModifier_kenaz_gear_location_3",
-		"RNGModifier_kenaz_gear_location_4"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_gear_location"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_hotel_room = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_hotel_room")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_hotel_room",
-	title = "RNGModifier_kenaz_hotel_room_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_hotel_room",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_hotel_room_1",
-		"RNGModifier_kenaz_hotel_room_2",
-		"RNGModifier_kenaz_hotel_room_3",
-		"RNGModifier_kenaz_hotel_room_4",
-		"RNGModifier_kenaz_hotel_room_5",
-		"RNGModifier_kenaz_hotel_room_6",
-		"RNGModifier_kenaz_hotel_room_7",
-		"RNGModifier_kenaz_hotel_room_8"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_hotel_room"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_bottle_location = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_bottle_location")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_bottle_location",
-	title = "RNGModifier_kenaz_bottle_location_title",
-	desc = "RNGModifier_kenaz_bottle_location_desc",
-	callback = "RNGModifier_kenaz_bottle_location",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_bottle_location_1",
-		"RNGModifier_kenaz_bottle_location_2",
-		"RNGModifier_kenaz_bottle_location_3",
-		"RNGModifier_kenaz_bottle_location_4"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_bottle_location"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_random_weak_floor = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_random_weak_floor")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_random_weak_floor",
-	title = "RNGModifier_kenaz_random_weak_floor_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_random_weak_floor",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_random_weak_floor_1",
-		"RNGModifier_kenaz_random_weak_floor_2"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_random_weak_floor"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_skylight = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_skylight")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_skylight",
-	title = "RNGModifier_kenaz_skylight_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_skylight",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_skylight_1",
-		"RNGModifier_kenaz_skylight_2"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_skylight"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_boss = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_boss")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_boss",
-	title = "RNGModifier_kenaz_boss_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_boss",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_boss_1",
-		"RNGModifier_kenaz_boss_2",
-		"RNGModifier_kenaz_boss_3"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_boss"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
-})
-
-MenuCallbackHandler.RNGModifier_kenaz_stairs = function(self, item)
-	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_stairs")
-	RNGModifier:Save()
-end
-MenuHelper:AddMultipleChoice({
-	id = "RNGModifier_kenaz_stairs",
-	title = "RNGModifier_kenaz_stairs_title",
-	desc = "RNGModifier_empty_desc",
-	callback = "RNGModifier_kenaz_stairs",
-	items = {
-		"RNGModifier_Default_One_Item",
-		"RNGModifier_kenaz_stairs_1",
-		"RNGModifier_kenaz_stairs_2",
-		"RNGModifier_kenaz_stairs_3"
-	},
-	value = RNGModifier:SafeGetData("kenaz", "_stairs"),
-	menu_id = "RNGModifier_kenaz_Options_Menu"
+	value = RNGModifier:SafeGetData("hvh", "_safe"),
+	menu_id = "RNGModifier_hvh_Options_Menu"
 })
