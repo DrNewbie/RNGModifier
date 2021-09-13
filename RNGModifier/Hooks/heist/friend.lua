b757a6d2ed510eb362a351805fe35531e3be1ced
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2021-09-13 17:52:05 -0300

    14.8 - The Assets Update

--- RNGModifier/Hooks/heist/friend.lua
+++ RNGModifier/Hooks/heist/friend.lua
@@ -213,3 +213,87 @@ MenuHelper:AddMultipleChoice({
 	value = RNGModifier:SafeGetData(_Curret_Heist, "_money_B"),
 	menu_id = "RNGModifier_friend_Options_Menu"
 })
+
+MenuCallbackHandler.RNGModifier_friend_ammo_bag = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_ammo_bag")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_friend_ammo_bag",
+	title = "RNGModifier_friend_ammo_bag_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_friend_ammo_bag",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_friend_ammo_bag_1",
+		"RNGModifier_friend_ammo_bag_2",
+		"RNGModifier_friend_ammo_bag_3",
+		"RNGModifier_friend_ammo_bag_4",
+		"RNGModifier_friend_ammo_bag_5"
+	},
+	value = RNGModifier:SafeGetData(_Curret_Heist, "_ammo_bag"),
+	menu_id = "RNGModifier_friend_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_friend_body_bag = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_body_bag")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_friend_body_bag",
+	title = "RNGModifier_friend_body_bag_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_friend_body_bag",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_friend_body_bag_1",
+		"RNGModifier_friend_body_bag_2",
+		"RNGModifier_friend_body_bag_3",
+		"RNGModifier_friend_body_bag_4",
+		"RNGModifier_friend_body_bag_5"
+	},
+	value = RNGModifier:SafeGetData(_Curret_Heist, "_body_bag"),
+	menu_id = "RNGModifier_friend_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_friend_grenade_case = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_grenade_case")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_friend_grenade_case",
+	title = "RNGModifier_friend_grenade_case_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_friend_grenade_case",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_friend_grenade_case_1",
+		"RNGModifier_friend_grenade_case_2",
+		"RNGModifier_friend_grenade_case_3",
+		"RNGModifier_friend_grenade_case_4",
+		"RNGModifier_friend_grenade_case_5"
+	},
+	value = RNGModifier:SafeGetData(_Curret_Heist, "_grenade_case"),
+	menu_id = "RNGModifier_friend_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_friend_doctor_bag = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_doctor_bag")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_friend_doctor_bag",
+	title = "RNGModifier_friend_doctor_bag_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_friend_doctor_bag",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_friend_doctor_bag_1",
+		"RNGModifier_friend_doctor_bag_2",
+		"RNGModifier_friend_doctor_bag_3",
+		"RNGModifier_friend_doctor_bag_4",
+		"RNGModifier_friend_doctor_bag_5"
+	},
+	value = RNGModifier:SafeGetData(_Curret_Heist, "_doctor_bag"),
+	menu_id = "RNGModifier_friend_Options_Menu"
+})
