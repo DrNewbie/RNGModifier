04210e96bf9a0d157339b4559f3ce5e6677e4714
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2021-11-27 12:35:38 -0300

    15.22

--- RNGModifier/Hooks/lua/coreelementlogicchance.lua
+++ RNGModifier/Hooks/lua/coreelementlogicchance.lua
@@ -51,6 +51,14 @@ function ElementLogicChance:on_executed(...)
 					self._chance = -999
 				end
 			end
+		elseif _level_id == "nightclub" then
+			if self._id == 104082 then
+				local _force_keycard = RNGModifier:SafeGetData(_level_id, "_force_keycard") or 0
+				_force_keycard = _force_keycard - 1
+				if _force_keycard == 1 then
+					self._chance = 999
+				end
+			end
 		elseif _level_id == "branchbank" then
 			if self._id == 102194 then
 				local _safe = RNGModifier:SafeGetData(_level_id, "_safe") or 0
