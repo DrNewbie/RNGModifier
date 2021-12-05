fdd4f567d5a8bc568b37db8ca4e159196c8c769d
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2021-12-05 19:05:25 -0300

    15.24

--- RNGModifier/Hooks/heist/firestarter_2.lua
+++ RNGModifier/Hooks/heist/firestarter_2.lua
@@ -17,11 +17,70 @@ MenuHelper:AddMultipleChoice({
 	callback = "RNGModifier_firestarter_2_randServerRoom",
 	items = {
 		"RNGModifier_Default_One_Item",
-		"RNGModifier_number_4_use_1",
-		"RNGModifier_number_4_use_2",
-		"RNGModifier_number_4_use_3",
-		"RNGModifier_number_4_use_4",
+		"RNGModifier_firestarter_2_randServerRoom_1",
+		"RNGModifier_firestarter_2_randServerRoom_2",
+		"RNGModifier_firestarter_2_randServerRoom_3",
+		"RNGModifier_firestarter_2_randServerRoom_4"
 	},
 	value = RNGModifier:SafeGetData("firestarter_2", "_randServerRoom"),
+	menu_id = "RNGModifier_firestarter_2_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_firestarter_2_safe = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_safe")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_firestarter_2_safe",
+	title = "RNGModifier_firestarter_2_safe_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_firestarter_2_safe",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_bool_4_true",
+		"RNGModifier_bool_4_false"
+	},
+	value = RNGModifier:SafeGetData("firestarter_2", "_safe"),
+	menu_id = "RNGModifier_firestarter_2_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_firestarter_2_computer = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_computer")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_firestarter_2_computer",
+	title = "RNGModifier_firestarter_2_computer_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_firestarter_2_computer",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_firestarter_2_computer_1",
+		"RNGModifier_firestarter_2_computer_2",
+		"RNGModifier_firestarter_2_computer_3",
+		"RNGModifier_firestarter_2_computer_4",
+		"RNGModifier_firestarter_2_computer_5",
+		"RNGModifier_firestarter_2_computer_6",
+		"RNGModifier_firestarter_2_computer_7",
+		"RNGModifier_firestarter_2_computer_8"
+	},
+	value = RNGModifier:SafeGetData("firestarter_2", "_computer"),
+	menu_id = "RNGModifier_firestarter_2_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_firestarter_2_loot = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_loot")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_firestarter_2_loot",
+	title = "RNGModifier_firestarter_2_loot_title",
+	desc = "RNGModifier_firestarter_2_loot_desc",
+	callback = "RNGModifier_firestarter_2_loot",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_bool_4_true"
+	},
+	value = RNGModifier:SafeGetData("firestarter_2", "_loot"),
 	menu_id = "RNGModifier_firestarter_2_Options_Menu"
 })
