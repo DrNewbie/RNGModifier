e6d29773a6f37d4026bdc2b49370f235790f7389
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2022-07-05 02:08:12 -0300

    15.5

--- RNGModifier/Hooks/heist/arm_und.lua
+++ RNGModifier/Hooks/heist/arm_und.lua
@@ -25,3 +25,62 @@ MenuHelper:AddMultipleChoice({
 	value = RNGModifier:SafeGetData("arm_und", "_rand_truck_amount"),
 	menu_id = "RNGModifier_arm_und_Options_Menu"
 })
+
+MenuCallbackHandler.RNGModifier_arm_und_escape = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_escape")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_arm_und_escape",
+	title = "RNGModifier_arm_und_escape_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_arm_und_escape",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_arm_und_escape_1",
+		"RNGModifier_arm_und_escape_2",
+		"RNGModifier_arm_und_escape_3",
+		"RNGModifier_arm_und_escape_4"
+	},
+	value = RNGModifier:SafeGetData("arm_und", "_escape"),
+	menu_id = "RNGModifier_arm_und_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_arm_und_ammo_bag = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_ammo_bag")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_arm_und_ammo_bag",
+	title = "RNGModifier_arm_und_ammo_bag_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_arm_und_ammo_bag",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_arm_und_ammo_bag_1",
+		"RNGModifier_arm_und_ammo_bag_2",
+		"RNGModifier_arm_und_ammo_bag_3"
+	},
+	value = RNGModifier:SafeGetData("arm_und", "_ammo_bag"),
+	menu_id = "RNGModifier_arm_und_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_arm_und_doctor_bag = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_doctor_bag")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_arm_und_doctor_bag",
+	title = "RNGModifier_arm_und_doctor_bag_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_arm_und_doctor_bag",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_arm_und_doctor_bag_1",
+		"RNGModifier_arm_und_doctor_bag_2",
+		"RNGModifier_arm_und_doctor_bag_3",
+		"RNGModifier_arm_und_doctor_bag_4"
+	},
+	value = RNGModifier:SafeGetData("arm_und", "_doctor_bag"),
+	menu_id = "RNGModifier_arm_und_Options_Menu"
+})
