8f8cda44eb4358c4ac22863f6522481b52079f33
Dom <38251670+Dom3062@users.noreply.github.com>
2022-11-20 16:55:59 +0100

    Fixed saved options not loaded correctly

--- RNGModifier/Hooks/lua/levelstweakdata.lua
+++ RNGModifier/Hooks/lua/levelstweakdata.lua
@@ -11,9 +11,13 @@ function LevelsTweakData:init(...)
     for _, heist in pairs(RNGModifier._heistlist) do
         local heist_data = self[heist]
         if heist_data and heist_data.name_id then
-            RNGModifier._data[heist] = {
-                name_id = heist_data.name_id
-            }
+            if RNGModifier._data[heist] then
+                RNGModifier._data[heist].name_id = name_id
+            else
+                RNGModifier._data[heist] = {
+                    name_id = heist_data.name_id
+                }
+            end
         end
     end
 end
