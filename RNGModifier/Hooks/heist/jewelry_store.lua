971071452a743dd080baeb3f0265eb25be00b4b0
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2023-01-09 15:14:08 -0300

    16.3

--- RNGModifier/Hooks/heist/jewelry_store.lua
+++ RNGModifier/Hooks/heist/jewelry_store.lua
@@ -23,3 +23,60 @@ MenuHelper:AddMultipleChoice({
 	value = RNGModifier:SafeGetData("jewelry_store", "_hotdog"),
 	menu_id = "RNGModifier_jewelry_store_Options_Menu"
 })
+
+MenuCallbackHandler.RNGModifier_jewelry_store_force_escape_day = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_force_escape_day")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_jewelry_store_force_escape_day",
+	title = "RNGModifier_jewelry_store_force_escape_day_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_jewelry_store_force_escape_day",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_bool_4_true",
+		"RNGModifier_bool_4_false"
+	},
+	value = RNGModifier:SafeGetData("jewelry_store", "_force_escape_day"),
+	menu_id = "RNGModifier_jewelry_store_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_jewelry_store_escape_day = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_escape_day")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_jewelry_store_escape_day",
+	title = "RNGModifier_jewelry_store_escape_day_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_jewelry_store_escape_day",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_jewelry_store_escape_day_1",
+		"RNGModifier_jewelry_store_escape_day_2",
+		"RNGModifier_jewelry_store_escape_day_3"
+	},
+	value = RNGModifier:SafeGetData("jewelry_store", "_escape_day"),
+	menu_id = "RNGModifier_jewelry_store_Options_Menu"
+})
+
+MenuCallbackHandler.RNGModifier_jewelry_store_escape = function(self, item)
+	RNGModifier:SafeSetData(item:value(), _Curret_Heist, "_escape")
+	RNGModifier:Save()
+end
+MenuHelper:AddMultipleChoice({
+	id = "RNGModifier_jewelry_store_escape",
+	title = "RNGModifier_jewelry_store_escape_title",
+	desc = "RNGModifier_empty_desc",
+	callback = "RNGModifier_jewelry_store_escape",
+	items = {
+		"RNGModifier_Default_One_Item",
+		"RNGModifier_jewelry_store_escape_1",
+		"RNGModifier_jewelry_store_escape_2",
+		"RNGModifier_jewelry_store_escape_3",
+		"RNGModifier_jewelry_store_escape_4"
+	},
+	value = RNGModifier:SafeGetData("jewelry_store", "_escape"),
+	menu_id = "RNGModifier_jewelry_store_Options_Menu"
+})
